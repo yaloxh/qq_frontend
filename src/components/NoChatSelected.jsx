7dba0bac30c3984cf1945de77cb95666de1cbfd4
ya takoy lox <maxmaxon0310@yandex.ru>
2025-02-11 11:34:51 +0400

    frontend added

---
+++ src/components/NoChatSelected.jsx
@@ -0,0 +1,29 @@
+import { MessageSquare } from "lucide-react";
+
+const NoChatSelected = () => {
+  return (
+    <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
+      <div className="max-w-md text-center space-y-6">
+        {/* Icon Display */}
+        <div className="flex justify-center gap-4 mb-4">
+          <div className="relative">
+            <div
+              className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
+             justify-center animate-bounce"
+            >
+              <MessageSquare className="w-8 h-8 text-primary " />
+            </div>
+          </div>
+        </div>
+
+        {/* Welcome Text */}
+        <h2 className="text-2xl font-bold">welcome to qq!</h2>
+        <p className="text-base-content/60">
+          it is the most secure open source messenger!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NoChatSelected;
